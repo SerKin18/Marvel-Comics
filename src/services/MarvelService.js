80a7f8d52cf23f4fc4678e7b42335e2374dcf1d7
Sergey <sergeykinyak@gmail.com>
2023-09-26 22:02:30 +0300

    class-components

---
+++ src/services/MarvelService.js
@@ -0,0 +1,41 @@
+
+
+
+class MarvelService {
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=81185e568d5b18b9b6e0b9e849bbbaaa';
+	_baseOffset = 210;
+	
+	getResource = async (url) => {
+		let res = await fetch(url);
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+		return await res.json();
+	}
+
+	getAllCharacters = async (offset = this._baseOffset) => {
+		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
+		return res.data.results.map(this._transformCharacter)
+	}
+
+	getCharacter = async (id) => {
+		const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+		return this._transformCharacter(res.data.results[0])
+	}
+
+	_transformCharacter = (char) => {
+
+		return {
+			comics: char.comics.items,
+			id: char.id,
+			name: char.name,
+			description: char.description ? `${char.description.slice(0, 200)}...` : 'There is no description for this character',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+		}
+	}
+}
+
+export default MarvelService
