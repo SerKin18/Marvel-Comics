3f519e0ecfa14ec9b22f697ceac9991cee3b52d6
Sergey <sergeykinyak@gmail.com>
2023-10-19 15:38:16 +0300

    charSearch by Formik & validate by Yup

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,29 +4,34 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from "../../resources/img/vision.png";
-
+import CharSearchForm from "../charSearchForm/charSearchForm";
 const MainPage = () => {
-  const [selectedChar, setChar] = useState(null);
+	const [selectedChar, setChar] = useState(null);
 
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
+	const onCharSelected = (id) => {
+		setChar(id);
+	};
 
-  return (
-    <>
-      <ErrorBoundary>
-        <RandomChar />
-      </ErrorBoundary>
-      <div className="char__content">
-        <ErrorBoundary>
-          <CharList onCharSelected={onCharSelected} />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <CharInfo charId={selectedChar} />
-        </ErrorBoundary>
-      </div>
-      <img className="bg-decoration" src={decoration} alt="vision" />
-    </>
-  );
+	return (
+		<>
+			<ErrorBoundary>
+				<RandomChar />
+			</ErrorBoundary>
+			<div className="char__content">
+				<ErrorBoundary>
+					<CharList onCharSelected={onCharSelected} />
+				</ErrorBoundary>
+				<div>
+					<ErrorBoundary>
+						<CharInfo charId={selectedChar} />
+					</ErrorBoundary>
+					<ErrorBoundary>
+						<CharSearchForm />
+					</ErrorBoundary>
+				</div>
+			</div>
+			<img className="bg-decoration" src={decoration} alt="vision" />
+		</>
+	);
 };
 export default MainPage;
